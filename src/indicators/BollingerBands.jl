b9ae4d0de34833fd236f8d31fef4a92b1d3297fe
takaymmt <dev@ymmt.cc>
2024-11-08 11:43:35 +0900

    Modify Bollinger Bands to use EMA and SMMA, instead of SMA

--- src/indicators/BollingerBands.jl
+++ src/indicators/BollingerBands.jl
@@ -19,19 +19,23 @@ Parameters:
 
 Returns a TSFrame with columns [:BB_Center, :BB_Upper, :BB_Lower]
 """
-function BB(ts::TSFrame, period::Int = 14; field::Symbol = :Close, ma_type::Symbol = :SMA, num_std = 2)
+function BB(ts::TSFrame, period::Int = 14; field::Symbol = :Close, num_std = 2, ma_type::Symbol = :SMA)
 	prices = ts[:, field]
-    if ma_type == :SMA
-        results = BB(prices, period; num_std = num_std)
-    end
+    results = BB(prices, period; num_std = num_std, ma_type = ma_type)
 	colnames = [:BB_Center, :BB_Upper, :BB_Lower]
 	return TSFrame(results, index(ts), colnames = colnames)
 end
 export BB
 
-function BB(prices::Vector{T}, period::Int = 14; num_std::Int = 2) where T
+function BB(prices::Vector{T}, period::Int = 14; num_std::Int = 2, ma_type::Symbol = :SMA) where T
     results = zeros(T, (length(prices),3))
-    masd = SMA_stats(prices, period)
+    if ma_type == :SMMA
+        masd = SMMA_stats(prices, period)
+    elseif ma_type == :EMA
+        masd = EMA_stats(prices, period)
+    else
+        masd = SMA_stats(prices, period)
+    end
     @inbounds for (i, price) in enumerate(prices)
         results[i,1] = masd[i,1]
         results[i,2] = masd[i,1] + num_std * masd[i,2]
